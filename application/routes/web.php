1f6f06fac9e165dadcfe150e3ca283be3f9c7a71
yohan <yohan@wvi.co.id>
2020-06-13 14:09:26 +0700

    remove useless before

--- application/routes/web.php
+++ application/routes/web.php
@@ -38,38 +38,15 @@ Route::group(['prefix' => 'admin'], function () {
 Route::group(['prefix' => 'admin', 'middleware' => ['auth','admin:ADMIN']], function () {
     Route::get('/', 'BackEnd\BackEndController@dashboard')->name('admin.dashboard');
     //Users
-    Route::get('/users', 'BackEnd\UserController@index')->name('users');
-    Route::get('/userData', 'BackEnd\UserController@indexData')->name('user.data');
-    Route::get('/userGet/{id}', 'BackEnd\UserController@getUser')->name('user.get');
-    Route::get('/userDetail/{id}', 'BackEnd\UserController@detail')->name('user.detail');
-    Route::post('/userSave', 'BackEnd\UserController@save')->name('user.save');
-    Route::post('/userDelete/{id}', 'BackEnd\UserController@delete')->name('user.delete');
-    Route::get('/userExport', 'BackEnd\UserController@export')->name('user.export');
+    Route::get('/users', 'BackEnd\UserController@index')->name('admin.users');
+    Route::get('/userData', 'BackEnd\UserController@indexData')->name('admin.user.data');
+    Route::get('/userGet/{id}', 'BackEnd\UserController@getUser')->name('admin.user.get');
+    Route::get('/userDetail/{id}', 'BackEnd\UserController@detail')->name('admin.user.detail');
+    Route::post('/userSave', 'BackEnd\UserController@save')->name('admin.user.save');
+    Route::post('/userDelete/{id}', 'BackEnd\UserController@delete')->name('admin.user.delete');
+    Route::get('/userExport', 'BackEnd\UserController@export')->name('admin.user.export');
     Route::get('/userSearch/{search}', 'BackEnd\UserController@search')->name('admin.user.search');
 
-    //divisions
-    Route::get('/divisions', 'BackEnd\DivisionController@index')->name('admin.divisions');
-    Route::get('/divisionData', 'BackEnd\DivisionController@indexData')->name('admin.division.data');
-    Route::get('/divisionGet/{id}', 'BackEnd\DivisionController@getData')->name('admin.division.get');
-    Route::post('/divisionSave', 'BackEnd\DivisionController@save')->name('admin.division.save');
-    Route::post('/divisionDelete/{id}', 'BackEnd\DivisionController@delete')->name('admin.division.delete');
-
-    //pic
-    Route::get('/pics', 'BackEnd\PicController@index')->name('admin.pics');
-    Route::get('/picData', 'BackEnd\PicController@indexData')->name('admin.pic.data');
-    Route::get('/picDetail/{id}', 'BackEnd\PicController@detail')->name('admin.pic.detail');
-    Route::get('/picDownload', 'BackEnd\PicController@download')->name('admin.pic.download');
-    Route::post('/picSave', 'BackEnd\PicController@save')->name('admin.pic.save');
-    Route::post('/picDelete/{id}', 'BackEnd\PicController@delete')->name('admin.pic.delete');
-
-    //reservation
-    Route::get('/reservations', 'BackEnd\ReservationController@index')->name('admin.reservations');
-    Route::get('/reservationData', 'BackEnd\ReservationController@indexData')->name('admin.reservation.data');
-    Route::get('/reservationDetail/{id}', 'BackEnd\ReservationController@detail')->name('admin.reservation.detail');
-    Route::get('/reservationDownload', 'BackEnd\ReservationController@download')->name('admin.reservation.download');
-    Route::post('/reservationSave', 'BackEnd\ReservationController@save')->name('admin.reservation.save');
-    Route::post('/reservationDelete/{id}', 'BackEnd\ReservationController@delete')->name('admin.reservation.delete');
-    Route::post('/reservationApprove', 'BackEnd\ReservationController@approve')->name('admin.reservation.approve');
 
     //notification
     Route::get('/notifications', 'BackEnd\NotificationController@index')->name('admin.notifications');
